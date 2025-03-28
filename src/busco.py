caf0f001a4deca039e6b3b531943d1c67fb671f6
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2025-03-28 16:32:31 +0100

    Update busco.py

--- src/busco.py
+++ src/busco.py
@@ -9,24 +9,24 @@ def run_busco(arguments, protein_sequences):
         lineage_outdir = outdir / lineage
         if not lineage_outdir.exists():
             lineage_outdir.mkdir(parents=True, exist_ok=True)
-            outfile = lineage_outdir / "run_{}".format(lineage) / "short_summary.txt"
-            cmd = "busco --cpu {} -i {} -o {} -m prot -l {} --force".format(arguments["Threads"],
+        outfile = lineage_outdir / "run_{}".format(lineage) / "short_summary.txt"
+        cmd = "busco --cpu {} -i {} -o {} -m prot -l {} --force".format(arguments["Threads"],
                                                                             protein_sequences,
                                                                             lineage_outdir,
                                                                             lineage)
-            print("XXXXXX")
+        print("XXXXXX")
+        print(outfile)
+        print("XXXXX")
+        if outfile.exists():
             print(outfile)
-            print("XXXXX")
-            if outfile.exists():
-                print(outfile)
-                msg = "Busco on lineage {} done already".format(lineage)
+            msg = "Busco on lineage {} done already".format(lineage)
+        else:
+            run_ = subprocess.run(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.DEVNULL)
+            if run_.returncode == 0:
+                msg = "BUSCO analysis with lineage {} run successfully".format(lineage)
             else:
-                run_ = subprocess.run(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.DEVNULL)
-                if run_.returncode == 0:
-                    msg = "BUSCO analysis with lineage {} run successfully".format(lineage)
-                else:
-                    msg = "BUSCO analysis with lineage {} Failed: \n {}".format(lineage, run_.stderr)
-                report[lineage] = {"command": cmd,
-                                   "status": msg,
-                                   "outfile": outfile}
+                msg = "BUSCO analysis with lineage {} Failed: \n {}".format(lineage, run_.stderr)
+            report[lineage] = {"command": cmd,
+                               "status": msg,
+                               "outfile": outfile}
     return report
