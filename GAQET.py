5d10f7f1d5bc56735d61d93081bf624f451f91f9
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2025-03-28 10:48:39 +0100

    added gffread to execution routine

--- GAQET.py
+++ GAQET.py
@@ -3,13 +3,14 @@ from argparse import RawTextHelpFormatter
 from pathlib import Path
 import sys
 
-from csv import DictReader
 from pathlib import Path
 from yaml import safe_load as load_yaml
 
 
+
 from src.agat import run_agat
-from src.busco import run_busco, run_gffread
+from src.busco import run_busco
+from src.gffread import run_gffread
 from src.LTR_retriever import create_outdir, run_suffixerator, run_harvest, run_finder, concatenate_outputs, run_LTR_retriever, run_LAI
 from src.stringtie import run_stringtie, run_gffcompare, calculate_annotation_scores
 from src.YAML import report_yaml_file
@@ -17,6 +18,9 @@ from src.YAML import report_yaml_file
 from pathlib import Path
 
 
+BULLET_OK = "\t✓\t"
+BULLET_FIX = "\tERROR!\t"
+HEADER = "-"*5
 
 def parse_arguments():
     description = '''\t\t\t###############\n\t\t\t##   GAQET   ##\n\t\t\t###############\n
@@ -67,6 +71,9 @@ def get_arguments():
     config_report = report_yaml_file(yaml)
     return yaml, config_report
 
+def emit_msg(string, log_fhand):
+    print(string)
+    log_fhand.write(string)
 
 def main():
     sys.tracebacklimit = 1
@@ -76,59 +83,77 @@ def main():
         basedir.mkdir(parents=True, exist_ok=True)
 
     log_fhand = open(basedir / "GAQET.log.txt", "w")
-    header = "\t\t\t###############\n\t\t\t##   GAQET   ##\n\t\t\t###############\n\n" + config_report
-    print(header)
-    log_fhand.write(header)
+    
+    header = "\t\t\t###############\n\t\t\t##   GAQET   ##\n\t\t\t###############\n\n" + config_report + "\n"
+    emit_msg(header, log_fhand)
+
     if "ERROR!" in config_report:
         raise RuntimeError("Check {} for details".format(str(basedir / "GAQET.log.txt")))
     
-    report
+    emit_msg("#Results will be stored at {}\n".format(basedir), log_fhand)
+   
+    emit_msg(HEADER + "Extracting CDS and protein sequences" + HEADER + "\n")
+    
+    gffread = run_gffread(arguments)
+    for kind, values in gffread.items():
+        status =  values["status"]
+        emit_msg("#{} extraction, command used: {}\n".format(kind, values["command"]))
+        if "Failed" in status:
+            emit_msg(BULLET_FIX + status + "\n")
+            emit_msg(HEADER + "GAQET has stopped working")
+            raise RuntimeError("GAQET has failed, {} for details".format(str(basedir / "GAQET.log.txt")))
+        else:
+            emit_msg(BULLET_OK + status + "\n")
 
-    for name, values in arguments["input"].items():
-        stats[name] = {}
-        name_dir = out_dir / name
-        values["output"] = name_dir
-        values["threads"] = arguments["threads"]
-        if not name_dir.exists():
-            name_dir.mkdir(parents=True, exist_ok=True)
 
-        agat_statistics = run_agat(values)
-        print(agat_statistics)
-        stats[name]["agat_statistics"] = agat_statistics
+    
+    
 
-        gffread_results = run_gffread(values)
-        print(gffread_results)
-        busco_results = run_busco(values)
-        print(busco_results)
-        stats[name]["busco_results"] = get_busco_results(busco_results)
+    # for name, values in arguments["input"].items():
+    #     stats[name] = {}
+    #     name_dir = out_dir / name
+    #     values["output"] = name_dir
+    #     values["threads"] = arguments["threads"]
+    #     if not name_dir.exists():
+    #         name_dir.mkdir(parents=True, exist_ok=True)
 
-        LAI_out_dir =  create_outdir(values)
-        print(LAI_out_dir)
-        values["LAI_dir"] = LAI_out_dir["out_fpath"]
-        suffixerator =  run_suffixerator(values)
-        if "returncode" in suffixerator:
-            if suffixerator["returncode"] == 1:
-                raise RuntimeError("Suffixerator has failed")
-        print(suffixerator)
-        harvest = run_harvest(values)
-        print(harvest)
-        finder = run_finder(values)
-        print(finder)
-        cat = concatenate_outputs(values)
-        print(cat)
-        LTR = run_LTR_retriever(values)
-        print(LTR)
-        LAI = run_LAI(values)
-        print(LAI)
-        stats[name]["LAI"] = LAI
+    #     agat_statistics = run_agat(values)
+    #     print(agat_statistics)
+    #     stats[name]["agat_statistics"] = agat_statistics
 
-        stringtie = run_stringtie(values)
-        print(stringtie)
-        gffcompare = run_gffcompare(values)
-        print(gffcompare)
-        annotation_scores = calculate_annotation_scores(values)
-        print(annotation_scores)
-        stats[name]["annotation_scores"] = annotation_scores
+    #     gffread_results = run_gffread(values)
+    #     print(gffread_results)
+    #     busco_results = run_busco(values)
+    #     print(busco_results)
+    #     stats[name]["busco_results"] = get_busco_results(busco_results)
+
+    #     LAI_out_dir =  create_outdir(values)
+    #     print(LAI_out_dir)
+    #     values["LAI_dir"] = LAI_out_dir["out_fpath"]
+    #     suffixerator =  run_suffixerator(values)
+    #     if "returncode" in suffixerator:
+    #         if suffixerator["returncode"] == 1:
+    #             raise RuntimeError("Suffixerator has failed")
+    #     print(suffixerator)
+    #     harvest = run_harvest(values)
+    #     print(harvest)
+    #     finder = run_finder(values)
+    #     print(finder)
+    #     cat = concatenate_outputs(values)
+    #     print(cat)
+    #     LTR = run_LTR_retriever(values)
+    #     print(LTR)
+    #     LAI = run_LAI(values)
+    #     print(LAI)
+    #     stats[name]["LAI"] = LAI
+
+    #     stringtie = run_stringtie(values)
+    #     print(stringtie)
+    #     gffcompare = run_gffcompare(values)
+    #     print(gffcompare)
+    #     annotation_scores = calculate_annotation_scores(values)
+    #     print(annotation_scores)
+    #     stats[name]["annotation_scores"] = annotation_scores
 
 
 if __name__ == "__main__":
--- src/gffread.py
+++ src/gffread.py
@@ -4,32 +4,33 @@ from pathlib import Path
 
 
 def run_gffread(config):
-    report = {"commands": [], "status": [], "outfiles": []}
-    modes = {"cds": "x", "proteins": "y"}
+    report = {"cds": {"mode": "x", "command": "", "status": "", "outfile": ""}, 
+              "proteins": {"mode": "y", "command": "", "status": "", "outfile": ""}}
     outdir = config["Basedir"] / "input_sequences"
+
     if not outdir.exists():
         outdir.mkdir(parents=True, exist_ok=True)
 
-    for mode, arg in modes.items():
-        outfile = outdir / "{}.{}.fasta".format(Path(config["Assembly"]).stem, mode)
-        cmd = "gffread -{} {} -g {} {}".format(arg,
+    for kind, values in report.items():
+        outfile = outdir / "{}.{}.fasta".format(Path(config["Assembly"]).stem, kind)
+        cmd = "gffread -{} {} -g {} {}".format(values["mode"],
                                                outfile,
                                                config["Assembly"],
                                                config["Annotation"])
         if outfile.exists():
-            msg = "{} sequences already extracted".format(mode)
+            msg = "{} sequences already extracted".format(kind)
         else:
         #Run BUSCO with command
             run_ = subprocess.run(cmd, shell=True, stderr=subprocess.PIPE)
         #Is process has gone well
             if run_.returncode == 0:
-                msg = "GFFread, mode {} run successfully".format(mode)
+                msg = "GFFread, mode {} run successfully".format(kind)
         #But if not
             else:
                 msg = "GFFread, mode {} Failed: \n {}".format(run_.stderr)
-        report["commands"].append(cmd)
-        report["status"].append(msg)
-        report["outfiles"].append(outfile)
+        report[kind]["command"].append(cmd)
+        report[kind]["status"].append(msg)
+        report[kind]["outfile"].append(outfile)
 
         #Return command, final message and output dir path
         return report
