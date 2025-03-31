bd17efa11a4f86a781587a65c908b3348b99e9d3
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2025-03-31 11:56:57 +0200

    Update GAQET.py

--- GAQET.py
+++ GAQET.py
@@ -170,6 +170,7 @@ def main():
             emit_msg(HEADER + "Running DeTEnGA"+ HEADER + "\n", log_fhand)
             detenga = run_detenga(arguments, gffread["proteins"]["outfile"], gffread["mrna"]["outfile"])
             for analysis, values in detenga.items():
+                print(analysis, values)
                 status = values["status"]
                 emit_msg("#{} command used: \n\t{}\n".format(analysis, values["command"]), log_fhand)
                 if "Failed" in status:
@@ -178,6 +179,7 @@ def main():
                     raise RuntimeError(error_msg)
                 else:
                     emit_msg(BULLET_OK + status + "\n", log_fhand)
+        
 
 
     
