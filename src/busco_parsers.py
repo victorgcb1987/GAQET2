44e30efbe9c7da33eac4e2fa90ad17dd30466ffe
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2025-04-01 13:00:13 +0200

    Update busco_parsers.py

--- src/busco_parsers.py
+++ src/busco_parsers.py
@@ -1,7 +1,6 @@
 def busco_stats(busco):
     results = {}
     for lineage, report in busco.items():
-        print(lineage)
         with open(report["outfile"]) as fhand:
             for line in fhand:
                 if "%" in line:
