8ea2e4333b843fe188c22dada92a1cec50b81436
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2025-04-01 16:02:53 +0200

    Update homology_parsers.py

--- src/homology_parsers.py
+++ src/homology_parsers.py
@@ -4,7 +4,7 @@ def protein_homology_stats(homology, num_transcripts):
         prots = []
         with open(values["outfile"]) as results_fhand:
             for line in results_fhand:
-                line = line.rstrip().split
+                line = line.rstrip().split()
                 evalue = float(line[10])
                 prot_id = line[0]
                 if evalue < 1e-20 and prot_id not in prots:
