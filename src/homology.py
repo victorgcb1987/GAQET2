69e924565c0d888ad77eb3baa2e34b07462cbcf5
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2025-04-01 14:55:57 +0200

    Update homology.py

--- src/homology.py
+++ src/homology.py
@@ -8,24 +8,22 @@ def run_protein_homology(config, protein_sequences):
     if not outdir.exists():
         outdir.mkdir(parents=True, exist_ok=True)
     for db in config["PROTHOMOLOGY_tags"]:
-        print(db)
-        tag =  list(db.keys())[0]
-        db_fpath = list[db.values()][0]
-        outfile = outdir / "{}.proteins.dmd.{}.o6.txt".format(config["ID"], tag)
-        cmd = "diamond blastp --threads {} --db {} --query {} --out {}".format(config["Threads"],
-                                                                               str(db_fpath),
-                                                                               str(protein_sequences),
-                                                                               str(outfile))
-        if outfile.is_file():
-            msg = "Protein homology analysis with {} already done".format(tag)
-        else:
-            run_ = subprocess.run(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.DEVNULL)
-        #Is process has gone well
-            if run_.returncode == 0:
-                msg = "Protein homology analysis with {} run successfully".format(tag)
-        #But if not
+        for tag, db_fpath in db.items():
+            outfile = outdir / "{}.proteins.dmd.{}.o6.txt".format(config["ID"], tag)
+            cmd = "diamond blastp --threads {} --db {} --query {} --out {}".format(config["Threads"],
+                                                                                str(db_fpath),
+                                                                                str(protein_sequences),
+                                                                                str(outfile))
+            if outfile.is_file():
+                msg = "Protein homology analysis with {} already done".format(tag)
             else:
-                msg = "Protein homology analysis with {} Failed: \n {}".format(tag, run_.stdout)
+                run_ = subprocess.run(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.DEVNULL)
+            #Is process has gone well
+                if run_.returncode == 0:
+                    msg = "Protein homology analysis with {} run successfully".format(tag)
+            #But if not
+                else:
+                    msg = "Protein homology analysis with {} Failed: \n {}".format(tag, run_.stdout)
         results[tag] = {"command": cmd, "status": msg, "outfile": outfile}
     print(results)
     return results
