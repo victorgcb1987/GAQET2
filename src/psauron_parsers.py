81eedfd7090b232ae37f4bb4d8aeb690ab39d0d7
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2025-06-02 15:26:40 +0200

    Update psauron_parsers.py

--- src/psauron_parsers.py
+++ src/psauron_parsers.py
@@ -9,4 +9,5 @@ def psauron_stats(psauron):
     with open(psauron["outfile"]) as fhand:
         for line in fhand:
             if "psauron score" in line:
+                print(line)
                 return {"PSAURON SCORE": line.strip().split()[-1]}        
