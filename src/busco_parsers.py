ad7a041b59ba4d5696be67843671f1bb7367c7e7
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2025-06-02 12:33:11 +0200

    fixes

--- src/busco_parsers.py
+++ src/busco_parsers.py
@@ -1,13 +1,14 @@
 from src.error_check import operation_failed
 
 def busco_stats(busco):
-    error = operation_failed(busco)
-    if error:
-        return error
     results = {}
     for lineage, report in busco.items():
-        with open(report["outfile"]) as fhand:
-            for line in fhand:
-                if "%" in line:
-                    results[lineage] = line.strip()
+        error = operation_failed(report)
+        if error:
+             results[lineage] = error
+        else:
+            with open(report["outfile"]) as fhand:
+                for line in fhand:
+                    if "%" in line:
+                        results[lineage] = line.strip()
     return results
