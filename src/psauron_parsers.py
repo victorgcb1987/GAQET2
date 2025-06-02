9e6f7ad65655b4b81cb092d9c893aa5ce3fbc2f5
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2025-06-02 15:29:14 +0200

    Update psauron_parsers.py

--- src/psauron_parsers.py
+++ src/psauron_parsers.py
@@ -1,15 +1,10 @@
 from src.error_check import operation_failed
 
 def psauron_stats(psauron):
-    print(psauron)
     error = operation_failed(psauron)
-    print(error)
-    if operation_failed:
+    if error:
         return {"PSAURON SCORE": error}
-    print(psauron["outfile"])
     with open(psauron["outfile"]) as fhand:
         for line in fhand:
-            print(line)
             if "psauron score" in line:
-                print(line)
                 return {"PSAURON SCORE": line.strip().split()[-1]}        
