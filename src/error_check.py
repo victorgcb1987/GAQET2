30a6ef59be672d775de46c56032e1a353b9d7de8
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2025-06-03 09:47:34 +0200

    added sequence length check

--- src/error_check.py
+++ src/error_check.py
@@ -10,4 +10,4 @@ def operation_failed(results):
 def correct_fasta_length(arguments):
     cmd = "sed -n '2p' {} | wc -c".format(arguments["Assembly"])
     seq_length = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
-    print(seq_length.stdout)
+    print(seq_length.stdout.decode().strip())
