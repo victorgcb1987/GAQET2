eb9f1c116abdf4d6ce6d4379ad763b46c493f11b
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2025-06-16 12:09:11 +0200

    added filter of sequences longer than 100k in TEsorter

--- src/detenga.py
+++ src/detenga.py
@@ -36,16 +36,26 @@ def run_detenga(config, protein_sequences, mrna_sequences):
     #Run TEsorter
 
     filtered_mRNA_outfile = outdir / "{}.mRNA.noNs.fasta".format(Path(config["Assembly"]).stem)
-    sequences_removed = []
+    sequences_removed_Ns = []
+    sequences_removed_length = []
     msg = ""
+    
     with open(filtered_mRNA_outfile, "w") as filtered_mRNA_outfhand:
         for record in SeqIO.parse(mrna_sequences.absolute(), "fasta"):
             if "N" not in record.seq.upper():
                 SeqIO.write(record, filtered_mRNA_outfhand, "fasta")
             else:
-                sequences_removed.append(record.id)
-    if sequences_removed:
-        msg = "Warning: the following sequences have Ns in the sequence and has been removed from TEsorter analysis:\n {}\n".format("\n".join(sequences_removed))
+                sequences_removed_Ns.append(record.id)
+            if len(record.seq) <= 100000:
+                SeqIO.write(record, filtered_mRNA_outfhand, "fasta")
+            else:
+                sequences_removed_length.append(record.id)
+
+    if sequences_removed_Ns:
+        msg = "Warning: the following sequences have Ns in the sequence and has been removed from TEsorter analysis:\n {}\n".format("\n".join(sequences_removed_Ns))
+    if sequences_removed_length:
+         msg = "Warning: the following sequences have more than 100k residues has been removed from TEsorter analysis:\n {}\n".format("\n".join(sequences_removed_length))
+
     base_dir = Path(os.getcwd())
     tesorter_outfile = outdir / "{}.{}.cls.tsv".format(filtered_mRNA_outfile.name, config["DETENGA_db"])
     cmd = "TEsorter {} -db {} -p {}".format(filtered_mRNA_outfile.absolute(), config["DETENGA_db"], str(config["Threads"]))
