35071e6f6834935e87f7a4c97632ab85e1c990c0
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2025-07-29 15:36:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
         name='gaqet',
-        version='1.10.13',
+        version='1.11.13',
         packages=find_packages(),
         install_requires=[],
         entry_points={
                      'console_scripts': [
-                                         'GAQET=GAQET.__main__:main',
+                                         'GAQET=GAQET2.GAQET:main',
+                                         'GAQET_PLOT=GAQET2.GAQET_PLOT:main'
                                                                 ],},
         author='Victor Garcia-Carpintero Burgos',
-        description='Genome Annotation Quality Evaluation Tool (GAQET2)',
+        description='Genome Annotation Quality Evaluation Tool (GAQET)',
         license='MIT',
         url='https://github.com/victorgcb1987/GAQET2',)
