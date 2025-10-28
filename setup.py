4992ec29db422ba483dbab235cc330c9a4049e82
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2025-10-28 13:13:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='gaqet',
-        version='1.11.15',
+        version='1.13.21',
         packages=find_packages(),
         include_package_data=True,
         install_requires=[],
