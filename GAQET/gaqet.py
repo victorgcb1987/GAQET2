fa253690b77913bb83abfb96bc6062bdcf62ae14
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2026-02-16 12:05:48 +0100

    BUSCO's lookup table updated

--- GAQET/gaqet.py
+++ GAQET/gaqet.py
@@ -36,7 +36,7 @@ BULLET_FIX = "\tERROR!\t"
 HEADER = "-"*5
 AVAILABLE_ANALYSIS = ["AGAT", "BUSCO", "PSAURON",
                       "DETENGA", "OMARK", "PROTHOMOLOGY"]
-VERSION = "v1.13.2"
+VERSION = "v1.13.3"
 
 
 def parse_arguments():
